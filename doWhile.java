2783a6fb7f4db0524718bf2486e1ff00adcfc53d
Akhilesh Kumar <akhilesh.k19857@gmail.com>
2026-03-07 23:45:45 +0530

    Add files via upload

---
+++ doWhile.java
@@ -0,0 +1,9 @@
+public class doWhile{
+    public static void main(String[] args) {
+        int i = 17;
+        do{
+            System.out.print(i+" ");
+            i++;
+        }while(i<=8);
+    }
+}
