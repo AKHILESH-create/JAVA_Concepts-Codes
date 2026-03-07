950457b8d8f614fa3bfbc280ca739d73aedf8712
Akhilesh Kumar <akhilesh.k19857@gmail.com>
2026-03-07 23:44:58 +0530

    Add files via upload

---
+++ chardatatype.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class chardatatype {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        char ch = 'A';
+        System.out.print(ch);
+
+    }
+}
---
+++ circleArea.java
@@ -0,0 +1,8 @@
+public class circleArea {
+    public static void main(String[] args){
+        double r = 5;
+        double a = 3.154*r*r;
+        System.out.print(a); 
+
+    }
+}
