1883eef7f4ad856c21ecc6fbf0a3d6547b78f4e3
Akhilesh Kumar <akhilesh.k19857@gmail.com>
2026-03-08 00:21:05 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,11 @@
+public class variable {
+    public static void main(String[] args){
+        int a = 25;
+        int b = 10;
+        int sum = a+b;
+        int difference = a-b;
+        System.out.println(sum);
+        System.out.print(difference);
+
+    }
+}
