8e220bc9b4333a34cf421ce2366fcf6d44b148d8
Akhilesh Kumar <akhilesh.k19857@gmail.com>
2026-03-08 00:16:53 +0530

    Add files via upload

---
+++ passingArguments.java
@@ -0,0 +1,9 @@
+public class passingArguments {
+    public static void greet(String name){
+        System.out.println("Good Morning" + name);
+
+    }
+    public static void main(String[] args){
+        greet("Alakh Sir");
+    }
+}
