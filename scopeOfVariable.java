a62b09087db449e903249293297f03166a3a444d
Akhilesh Kumar <akhilesh.k19857@gmail.com>
2026-03-11 00:38:45 +0530

    Add files via upload

---
+++ scopeOfVariable.java
@@ -0,0 +1,15 @@
+public class scopeOfVariable{
+    static int i;
+    public static void main(String[] args){
+        i=16;
+        System.out.println(i);
+        int i = 23;
+        System.out.println(i);
+        fun();
+        
+
+    }
+    public static void fun(){
+        System.out.println(i);
+    }
+}
